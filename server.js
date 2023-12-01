97626febcd4f1645fb0ada5f6b6f0b10b2d147d8
Eyrma <erotstein@outlook,com>
2023-11-30 20:16:10 -0800

    Fixing structure

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
+
+
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+
+// MIDDLEWARE
+app.use(methodOverride('_method'))
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -15,8 +27,13 @@ app.get('/', (req, res) => {
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
   
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
