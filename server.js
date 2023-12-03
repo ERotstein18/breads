7d8315382d7772666e95659a36ff3641852bea12
Eyrma <erotstein@outlook,com>
2023-12-03 11:14:35 -0800

    adding mongoose

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 // MIDDLEWARE
 app.use(methodOverride('_method'))
@@ -31,6 +34,7 @@ app.get('/', (req, res) => {
 app.get('*', (req, res) => {
   res.send('404')
 })
+
 
 // LISTEN
 app.listen(PORT, () => {
