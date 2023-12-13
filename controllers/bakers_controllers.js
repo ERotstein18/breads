ad7082d63664ac596ca779b7bd5b5d6fa0c99a24
Eyrma <erotstein@outlook,com>
2023-12-12 18:43:21 -0800

    update

--- controllers/bakers_controllers.js
+++ controllers/bakers_controllers.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const baker = express.Router()
-const Baker = require('../models/baker.js')
+const baker = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
 
 baker.get('/data/seed', (req, res) => {
-    Baker.insertMany(bakerSeedData)
+    baker.insertMany(bakerSeedData)
         .then(res.redirect('/breads'))
 })
 
